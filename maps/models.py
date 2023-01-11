2fe1e7ddf822529b9db6022f10e5e0749e2fa2b6
iheehee <iheeheedev@gmail.com>
2023-01-11 12:54:21 +0900

    mapupdate building

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Maps(models.Model):
+    name = models.CharField(max_length=140)
+    address = models.CharField(max_length=140)
+    tel = models.CharField(max_length=140)
+    lat = models.DecimalField(max_digits=10, decimal_places=6)
+    lng = models.DecimalField(max_digits=10, decimal_places=6)
+
+
+class Shop(models.Model):
+    category = models.CharField(max_length=140)
--- maps/views.py
+++ maps/views.py
@@ -3,19 +3,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class SearchShop(APIView):
-    def map_data(self, page):
-        "서울시 맵데이터 api 호출"
+class MapUpdate(APIView):
+
+    ss = save_map_data()
+
+    def save_map_data(self, results):
+        return 0
+
+    def map_request(self, request):
         key = "f79ef2eeb2b947f9bf3510e0e848a2be"
         response = requests.get(
-            f"https://map.seoul.go.kr/smgis/apps/theme.do?cmd=getContentsList&key={key}&page_size=10&page_no={page}&coord_x=126.974695&coord_y=37.564150&distance=100000&search_type=0&search_name=&theme_id=11103395&content_id=&subcate_id="
+            f"https://map.seoul.go.kr/smgis/apps/theme.do?cmd=getContentsList&key={key}&page_size=10&page_no=1&coord_x=126.974695&coord_y=37.564150&distance=100000&search_type=0&search_name=&theme_id=11103395&content_id=&subcate_id="
         )
         results = response.json()
 
-        return results
-
+        return 0
 
     def get(self, request):
-        page= 1
-        return Response(data=self.map_data(page))
-
+        "서울시 맵데이터 api 호출 -> DB에 저장"
+        return
